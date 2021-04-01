4cf276aedab3540ffadad96b3e50a26a9965e228
田知勲 <j-jihoon@winas.jp>
2021-04-01 11:39:31 +0900

    개발 준비

--- youtubeView/youtubeView/ViewController.swift
+++ youtubeView/youtubeView/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showVideoBtn: UIButton!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initView()
     }
-
-
 }
 
+extension ViewController {
+    func initView() {
+        image.layer.cornerRadius = 10
+        showVideoBtn.addTarget(self, action: #selector(didTabImage), for: .touchUpInside)
+    }
+}
+
+extension ViewController {
+    @objc func didTabImage() {
+        let storyboard = UIStoryboard(name: "VideoViewController", bundle: nil)
+        guard let vc = storyboard.instantiateViewController(withIdentifier: "VideoViewController") as? VideoViewController else {fatalError()}
+        vc.modalPresentationStyle = .overCurrentContext
+        present(vc, animated: true, completion: nil)
+    }
+}
---
+++ youtubeView/youtubeView/videoView/VideoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VideoViewController.swift
+//  youtubeView
+//
+//  Created by Jh's Macbook Pro on 2021/04/01.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func dismissBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
