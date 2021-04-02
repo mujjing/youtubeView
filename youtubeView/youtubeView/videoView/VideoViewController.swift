d31ad6b6f2fbdfe7d4e96a085c193163208e70d0
田知勲 <j-jihoon@winas.jp>
2021-04-02 19:49:01 +0900

    화면 애니메이션 다듬기

--- youtubeView/youtubeView/videoView/VideoViewController.swift
+++ youtubeView/youtubeView/videoView/VideoViewController.swift
@@ -41,9 +41,7 @@ class VideoViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 0.3) {
-            self.baseBackGrountView.alpha = 1
-        }
+
     }
     
     @IBAction func dismissBtn(_ sender: Any) {
@@ -58,7 +56,6 @@ extension VideoViewController {
         videoImageView.addGestureRecognizer(panGesture)
         
         imageViewCenterY = videoImageView.center.y
-        print("videoImageMaxY : ", videoImageMaxY)
     }
 }
 
@@ -79,49 +76,18 @@ extension VideoViewController {
             
             imageView.transform = CGAffineTransform(translationX: 0, y: move.y) // x값은 변하지 않은채 y값만 변하면서 이미지가 움직인다
             videoImageBackView.transform = CGAffineTransform(translationX: 0, y: move.y)
+            
             //이미지뷰 좌우 padding 설정
-            
-            let movingConstant = move.y / 30
-            
-            if movingConstant <= 12 {
-                videoImageViewTrailingConstraint.constant = movingConstant
-                videoImageViewLeadingConstraint.constant = movingConstant
-                
-                backViewTrailingContraint.constant = movingConstant
-            }
+            self.adjustPaddingChange(move: move)
             
             // 280(최대값) - 70(최소값) = 210 -> 화면 작아질 때 이미지 뷰 높이
-            let parentViewHeight = self.view.frame.height
-            let heightRatio = 210 / (parentViewHeight - (parentViewHeight / 6))
-            let moveHeight = move.y * heightRatio
-            
-            backViewTopConstraint.constant = move.y //이미지 움직일 때 backView Top부분 제약조건 이미지 뷰 y위치랑 같게 변동
-            videoImageViewHeightConstraint.constant = 280 - moveHeight
-            describeViewTopConstraint.constant = move.y * 0.8//이미지 움직일 때 decripbeView Top부분 제약조건 이미지 뷰 y변한 만큼 같이 위치 변동
-            
-            let bottomMoveY = parentViewHeight - videoImageMaxY
-            let bottomMoveRatio = bottomMoveY / videoImageMaxY
-            let bottomMoveConstant = move.y * bottomMoveRatio
-            backViewBottomConstraint.constant = bottomMoveConstant
+            self.adjustHeightChange(move: move)
             
             // alpha값 설정
-            let alphaRatio = move.y / ( parentViewHeight / 2)
-            describeView.alpha = 1 - alphaRatio
-            baseBackGrountView.alpha = 1 - alphaRatio
+            self.adjustAlphaChange(move: move)
             
             //이미지뷰 가로 폭 움직임 150(최소값)
-            let originalWidth = self.view.frame.width
-            let miniImageViewTrailingConstant = -(originalWidth - (150 + 12))
-            let constant = originalWidth - move.y
-            
-            if minimumImageViewTrailingConstant > constant {
-                videoImageViewTrailingConstraint.constant = -miniImageViewTrailingConstant
-                return
-            }
-            
-            if constant < -12 {
-                videoImageViewTrailingConstraint.constant = -constant
-            }
+            self.adjustWidthChange(move: move)
             
         } else if gesture.state == .ended {//움직임이 멈췄을 때
             
@@ -130,10 +96,69 @@ extension VideoViewController {
                     self.backToIdentityAllView(imageView: imageView as! UIImageView)
                 }) // 스프링 처럼 튀어오르는 효과주기
             } else {
-                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.6, options: []) {
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: []) { //화면이 작아지는 처리
                     self.moveToBottom(imageView: imageView as! UIImageView)
+                } completion: { _ in //화면이 작아지는 처리가 끝났을 때 하는 처리
+                    
+                    UIView.animate(withDuration: 0.2) {
+                        self.videoImageView.isHidden = true
+                        self.videoImageBackView.isHidden = true
+                        
+                        let image = self.videoImageView.image
+                        let userinfo:[String : UIImage?] = ["image": image]
+                        NotificationCenter.default.post(name: .init("thumbnailImage"), object: nil, userInfo: userinfo as [AnyHashable : Any])
+                    } completion: { _ in
+                        self.dismiss(animated: false, completion: nil)
+                    }
                 }
             }
+        }
+    }
+    
+    private func adjustPaddingChange(move: CGPoint) {
+        let movingConstant = move.y / 30
+        
+        if movingConstant <= 12 {
+            videoImageViewTrailingConstraint.constant = movingConstant
+            videoImageViewLeadingConstraint.constant = movingConstant
+            
+            backViewTrailingContraint.constant = movingConstant
+        }
+    }
+    
+    private func adjustHeightChange(move: CGPoint) {
+        let parentViewHeight = self.view.frame.height
+        let heightRatio = 210 / (parentViewHeight - (parentViewHeight / 6))
+        let moveHeight = move.y * heightRatio
+        
+        backViewTopConstraint.constant = move.y //이미지 움직일 때 backView Top부분 제약조건 이미지 뷰 y위치랑 같게 변동
+        videoImageViewHeightConstraint.constant = 280 - moveHeight
+        describeViewTopConstraint.constant = move.y * 0.8//이미지 움직일 때 decripbeView Top부분 제약조건 이미지 뷰 y변한 만큼 같이 위치 변동
+        
+        let bottomMoveY = parentViewHeight - videoImageMaxY
+        let bottomMoveRatio = bottomMoveY / videoImageMaxY
+        let bottomMoveConstant = move.y * bottomMoveRatio
+        backViewBottomConstraint.constant = bottomMoveConstant
+    }
+    
+    private func adjustAlphaChange(move: CGPoint) {
+        let alphaRatio = move.y / ( self.view.frame.height / 2)
+        describeView.alpha = 1 - alphaRatio
+        baseBackGrountView.alpha = 1 - alphaRatio
+    }
+    
+    private func adjustWidthChange(move: CGPoint) {
+        let originalWidth = self.view.frame.width
+        let miniImageViewTrailingConstant = -(originalWidth - (150 + 12))
+        let constant = originalWidth - move.y
+        
+        if minimumImageViewTrailingConstant > constant {
+            videoImageViewTrailingConstraint.constant = -miniImageViewTrailingConstant
+            return
+        }
+        
+        if constant < -12 {
+            videoImageViewTrailingConstraint.constant = -constant
         }
     }
     
